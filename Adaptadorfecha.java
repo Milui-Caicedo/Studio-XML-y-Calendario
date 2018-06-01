965aff21ee122dd94e286f799f31b3b72287c2d5
Milui-Caicedo <39670030+Milui-Caicedo@users.noreply.github.com>
2018-06-01 17:58:41 -0500

    Add files via upload

---
+++ Adaptadorfecha.java
@@ -0,0 +1,97 @@
+package lu.dmi.icesi.studio;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+/**
+ * Created by estudiante on 31/05/18.
+ */
+
+public class Adaptadorfecha extends BaseAdapter {
+
+    Activity activity;
+    ArrayList<Evento> eventodeldia;
+    ArrayList <Evento> eventos;
+    TextView nombreevento, inicio, fin, fecha;
+    ImageButton borrar;
+
+    FirebaseDatabase database;
+    DatabaseReference referenceArch;
+
+    public Adaptadorfecha(Activity activity){
+        database = FirebaseDatabase.getInstance();
+        referenceArch = database.getReference("Eventos");
+        this.activity = activity;
+        eventodeldia = new ArrayList<Evento>();
+        eventos = new ArrayList<Evento>();
+        nombreevento = new TextView(activity);
+    }
+
+
+    @Override
+    public int getCount() {
+        return eventos.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return eventos.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int i, View view, ViewGroup viewGroup) {
+        LayoutInflater li = activity.getLayoutInflater();
+        View ver = li.inflate(R.layout.fecharenglon,null);
+
+        nombreevento = (TextView) ver.findViewById(R.id.nombredeentrega);
+        inicio = (TextView) ver.findViewById(R.id.inicio);
+        fin = (TextView) ver.findViewById(R.id.fin);
+        fecha = (TextView) ver.findViewById(R.id.fechadeentrega);
+
+        borrar = (ImageButton) ver.findViewById(R.id.removefgh);
+
+        nombreevento.setText(eventos.get(i).getNombreevento());
+        inicio.setText(eventos.get(i).getInicio());
+        fin.setText(eventos.get(i).getFin());
+        fecha.setText(eventos.get(i).getFecha());
+
+        borrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eventos.remove(eventos.get(i));
+                String aquitar = eventos.get(i).getId();
+                DatabaseReference ref = database.getReference("Eventos").child(aquitar);
+                ref.setValue(null);
+                notifyDataSetChanged();
+
+            }
+        });
+
+        return ver;
+    }
+
+    public void addevento(Evento nuevo){
+        eventos.add(nuevo);
+        notifyDataSetChanged();
+        String id = referenceArch.push().getKey();
+        nuevo.setId(id);
+        DatabaseReference ref = database.getReference("Eventos").child(id);
+        ref.setValue(nuevo);
+    }
+}
